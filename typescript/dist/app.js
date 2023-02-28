6ade6187b071a5c2f653951844256c481b0846fb
Adriano M. da Rosa <71461040+adriano-rosa@users.noreply.github.com>
2023-02-27 21:06:56 -0300

    Configuração e Primeiro exemplo typescript
    1 - Instalar o compilador do typescript:
    npm install typescript -g
    tsc -v (identifica a versão)
    
    2 - Para compilar um arquivo somente:
    tsc app.ts (app.ts é o nome do arquivo) já cria o arquivo app.ts
    
    3 - tsc -w
    (monitora as alterações em tempo de execução)
    
    Separar os arquivos TS dos arquivos JS no projeto, configurar o tsconfig.json
    
    Para realizar as execuções no arquivo app.ts usar o comando:
    node dist/app

---
+++ typescript/dist/app.js
@@ -0,0 +1,9 @@
+var message = "Help me, Obi-Wan Kenobi. You´re my only hope!";
+console.log(message);
+var episode = 4;
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next episode is " + episode);
+var favoriteDroitd;
+favoriteDroitd = 'BB-8';
+console.log("My favorite droid is " + favoriteDroitd);
