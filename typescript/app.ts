2b314f049ca405570e7d9bd8eb71c3039ab312b2
Adriano M. da Rosa <71461040+adriano-rosa@users.noreply.github.com>
2023-02-28 22:33:02 -0300

    Funções em typescript

--- typescript/app.ts
+++ typescript/app.ts
@@ -9,3 +9,21 @@ console.log("Next episode is " + episode)
 let favoriteDroitd
 favoriteDroitd = 'BB-8'
 console.log("My favorite droid is " + favoriteDroitd)
+
+let isEnoughtToBeatMF = function (parsecs: number): boolean {
+    return parsecs < 12
+}
+
+let distance = 11
+console.log(`Is ${distance} parsecs enough to beat Millenium Falcon? ${isEnoughtToBeatMF(distance) ? 'YES': 'NO'} `)
+
+// lado esquerdo ficam os parâmetros => lado direito a implementação
+           let call = (name: string) => console.log(`Do you copy, ${name}?`)
+           call('R2')
+
+function inc (speed: number, inc: number = 1 ) : number {
+    return speed + inc
+}
+
+console.log(`inc (5, 1) = ${inc(5, 1)}`)
+console.log(`inc (5) = ${inc(5)}`)
--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -7,3 +7,17 @@ console.log("Next episode is " + episode);
 var favoriteDroitd;
 favoriteDroitd = 'BB-8';
 console.log("My favorite droid is " + favoriteDroitd);
+var isEnoughtToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 11;
+console.log("Is ".concat(distance, " parsecs enough to beat Millenium Falcon? ").concat(isEnoughtToBeatMF(distance) ? 'YES' : 'NO', " "));
+// lado esquerdo ficam os parâmetros => lado direito a implementação
+var call = function (name) { return console.log("Do you copy, ".concat(name, "?")); };
+call('R2');
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc (5, 1) = ".concat(inc(5, 1)));
+console.log("inc (5) = ".concat(inc(5)));
